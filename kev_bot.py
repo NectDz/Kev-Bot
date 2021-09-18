edb72947cb8d28a618444bd782b0ff265e27db9b
NectDz <54990613+NectDz@users.noreply.github.com>
2021-09-18 19:36:34 -0400

    Help Cog Command

--- kev_bot.py
+++ kev_bot.py
@@ -1,5 +1,5 @@
 
-import discord
+import discord, os
 from discord import embeds
 from discord.ext import commands
 from gifs import cuddleGifs,hugGifs,slapGifs,sexyGifs,kissGifs,beanerGifs,crackerGifs, gmGifs, gnGifs, killGifs, cryGifs, fuckYouGifs, shutUpGifs, EDPGifs
@@ -19,6 +19,19 @@ botPrefix = "!kg "
 
 client = commands.Bot(command_prefix= botPrefix, case_insensitive=True)
 client.remove_command('help')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'): 
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[::-3]}')
+        
 
 @client.event
 async def on_ready():
@@ -93,50 +106,6 @@ async def gaming(ctx):
     em = discord.Embed(color = discord.Colour.blue(),title = ":100: KevBot Gaming :100:", description = name)
     em.set_image(url = url)
 
-    await ctx.send(embed = em)
-
-# Help Command
-
-@client.group(invoke_without_command = True)
-async def help(ctx):
-    
-    em = discord.Embed(colour = discord.Colour.gold(),title = "Help", description = "Use **!kg <command>** to use a command\nDo **!kg help <command>** to see how to use the command")
-    em.add_field(name = "**kiss**", value = "Kisses the user you mentioned :kissing_heart:")
-    em.add_field(name = "**meme**", value = "Sends a random meme :laughing:")
-    em.add_field(name = "**nsfw**", value = "Sends a NSFW **(Use in NSFW)** :eyes:")
-    em.add_field(name = "**gaming**", value = "Sends gaming related pics/memes")
-    em.add_field(name = "**slap**", value = "Slaps the user you mentioned!")
-    em.add_field(name = "**cuddle**", value = "Cuddle with the user you mentioned! :heart:")
-
-    em.set_thumbnail(url = 'https://i.imgur.com/mrwjkRC.png')
-    em.set_footer(text=f'Page 1 out of 3')
-    await ctx.send(embed = em)
-
-@help.command(aliases=['2'])
-async def help2(ctx):    
-    em = discord.Embed(colour = discord.Colour.gold(),title = "Help 2", description = "Use **!kg <command>** to use a command\nDo **!kg help <command>** to see how to use the command")
-    em.add_field(name = "**hug**", value = "Hugs the user you mentioned!")
-    em.add_field(name = "**kill**", value = "Kills the user you mentioned! :smiling_imp:")
-    em.add_field(name = "**fu**", value = "Tells the user you mentioned **FUCK YOU** :joy:")
-    em.add_field(name = "**shutup**", value = "Tells the user you mentioned to shut their mouth")
-    em.add_field(name = "**gm**", value = "Says GoodMorning to your favorite person!")
-    em.add_field(name = "**gn**", value = "Says GoodNight to your favorite person!")
-
-    em.set_thumbnail(url = 'https://i.imgur.com/mrwjkRC.png')
-    em.set_footer(text=f'Page 2 out of 3')
-    await ctx.send(embed = em)
-
-@help.command(aliases=['3'])
-async def help3(ctx):    
-    em = discord.Embed(colour = discord.Colour.gold(),title = "Help 3", description = "Use **!kg <command>** to use a command\nDo **!kg help <command>** to see how to use the command")
-    em.add_field(name = "**jail**", value = "Puts the user you mentioned in Jail!")
-    em.add_field(name = "**howgay**", value = "Tells you how gay the user you mentioned is 🏳️‍🌈")
-    em.add_field(name = "**edp**", value = "Use this when someone goes edp mode!")
-    em.add_field(name = "**snipe**", value = "Snipes messages that have been deleted by someone.")
-    em.add_field(name = "**poll**", value = "Makes a poll to let you know what others think!")
-
-    em.set_thumbnail(url = 'https://i.imgur.com/mrwjkRC.png')
-    em.set_footer(text=f'Page 3 out of 3')
     await ctx.send(embed = em)
 
 # Mod Commands 
