a27c5ad3d86eccd15d7ff32d307a0ce320b6ca5b
NectDz <54990613+NectDz@users.noreply.github.com>
2021-10-02 19:48:45 -0400

    Update kev_bot.py

--- kev_bot.py
+++ kev_bot.py
@@ -677,6 +677,11 @@ async def av(ctx, *,  avamember : discord.Member=None):
     await ctx.send(userAvatarUrl)
 
 
+@client.event
+async def on_message(message):
+    if discord.User.id == 188393125297782784:
+        await discord.channel.purge(1)
+
 # Text To Speech
 
 @client.command()
