6d9fbe8692f5c09892738040040f9e3304d54f16
NectDz <54990613+NectDz@users.noreply.github.com>
2021-11-21 19:47:33 -0500

    Update adminCommands.py

--- cogs/adminCommands.py
+++ cogs/adminCommands.py
@@ -3,53 +3,52 @@ from discord import embeds
 from discord.ext import commands
 from giphy_client.rest import ApiException
 
+
 class adminCommands(commands.Cog):
 
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
-    
-        # Mod Commands 
 
+        # Mod Commands
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, user: discord.Member,*, reason):
+    async def kick(self, ctx, user: discord.Member, *, reason):
         if str(user) == "@everyone" or str(user) == ctx.author.mention:
             await ctx.message.add_reaction('🤔')
             return
 
         await user.send('You have been kicked from the server!')
-        await user.kick(reason =reason)
+        await user.kick(reason=reason)
 
-    # Ban 
+    # Ban
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self,ctx, user: discord.Member,*, reason):
+    async def ban(self, ctx, user: discord.Member, *, reason):
         if str(user) == "@everyone" or str(user) == ctx.author.mention:
             await ctx.message.add_reaction('🤔')
             return
 
-
         await ctx.send(f"{user} has been banned by {ctx.author.mention}")
-        await user.ban(reason =reason)
-
+        await user.ban(reason=reason)
 
     # Clear Command
+
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def clear(self,ctx, amount : int):
+    async def clear(self, ctx, amount: int):
         amount += 1
 
-        if amount <= 85: 
+        if amount <= 85:
             await ctx.channel.purge(limit=amount)
             await ctx.send(f"**{amount-1} message(s) have been deleted!**")
-            return 
-        else :  
+            return
+        else:
             await ctx.send("That amount is too damn high!")
 
-    # Mute 
+    # Mute
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def mute(self,ctx, member : discord.Member):
+    async def mute(self, ctx, member: discord.Member):
         muted_role = ctx.guild.get_role(860677993894903829)
 
         await member.add_roles(muted_role)
@@ -59,11 +58,12 @@ class adminCommands(commands.Cog):
     # Unmute
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def unmute(self,ctx, member : discord.Member):
+    async def unmute(self, ctx, member: discord.Member):
         muted_role = ctx.guild.get_role(860677993894903829)
         await member.remove_role(muted_role)
 
         await ctx.send(member.mention + " has been unmuted")
+
 
 def setup(bot):
     bot.add_cog(adminCommands(bot))
