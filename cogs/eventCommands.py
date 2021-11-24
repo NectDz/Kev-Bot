971f740e2cc34a9ceaa59e3acf573f45ab2f58d3
NectDz <54990613+NectDz@users.noreply.github.com>
2021-11-24 13:18:06 -0500

    Update eventCommands.py

--- cogs/eventCommands.py
+++ cogs/eventCommands.py
@@ -11,61 +11,69 @@ import asyncio
 snipe_message_author = {}
 snipe_message_content = {}
 
+
 class eventCommands(commands.Cog):
 
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_member_join(self,member):
+    async def on_member_join(self, member):
         pass
 
     @commands.Cog.listener()
-    async def on_member_remove(self,member):
+    async def on_member_remove(self, member):
         print(f'{member} is gone!.')
 
     # Hello Event
 
     @commands.Cog.listener()
-    async def on_message(self,message):
+    async def on_message(self, message):
 
-        hellos = ['hi','hello','HELLO','hey','heyy','heyyy','hai','sup','whats up', 'Good Morning','GOOD MORNING','good morning','GoodMorning']
+        if "img_8905.jpg" in message.content.lower():
+            await message.channel.purge(limit=1)
 
-        if message.content.lower() in hellos: 
+        hellos = ['hi', 'hello', 'HELLO', 'hey', 'heyy', 'heyyy', 'hai', 'sup',
+                  'whats up', 'Good Morning', 'GOOD MORNING', 'good morning', 'GoodMorning']
+
+        if message.content.lower() in hellos:
             await message.channel.send(f"Howdy there!")
-        
-        if message.content.lower() == "the younger the better": 
+
+        if message.content.lower() == "the younger the better":
             await message.channel.send(f"Exactly!")
 
     # CoolDowns
 
     @commands.Cog.listener()
-    async def on_command_error(self,ctx, error):
-        if isinstance(error, commands.errors.CommandOnCooldown):  
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.errors.CommandOnCooldown):
             # isinstance is used to compare 2 thing. If they are the same it returns True in this case were comparing if the error is equal to the other error
-            em = discord.Embed(color = discord.Colour.blue(),
-            title = "Slow down there partner...", description = 'The command **{}** is still on cooldown for {:.2f}'.format(ctx.command.name, error.retry_after))
-            await ctx.send(embed = em)
+            em = discord.Embed(color=discord.Colour.blue(),
+                               title="Slow down there partner...", description='The command **{}** is still on cooldown for {:.2f}'.format(ctx.command.name, error.retry_after))
+            await ctx.send(embed=em)
 
     # Commands/Events
 
     @commands.Cog.listener()
-    async def on_message_delete(self,message):
+    async def on_message_delete(self, message):
         snipe_message_author[message.channel.id] = message.author
         snipe_message_content[message.channel.id] = message.content
         await asyncio.sleep(60)
         del snipe_message_author[message.channel.id]
         del snipe_message_content[message.channel.id]
-    
+
     @commands.command()
-    async def snipe(self,ctx):
-        channel = ctx.channel 
+    async def snipe(self, ctx):
+        channel = ctx.channel
         try:
-            snipeEmbed = discord.Embed(title=f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
-            snipeEmbed.set_footer(text=f"Sent By {snipe_message_author[channel.id]}")
-            await ctx.send(embed = snipeEmbed)
+            snipeEmbed = discord.Embed(
+                title=f"Last deleted message in #{channel.name}", description=snipe_message_content[channel.id])
+            snipeEmbed.set_footer(
+                text=f"Sent By {snipe_message_author[channel.id]}")
+            await ctx.send(embed=snipeEmbed)
         except:
             await ctx.send(f"There are no deleted messages in #{channel.name}")
+
 
 def setup(bot):
     bot.add_cog(eventCommands(bot))
