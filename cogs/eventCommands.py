66be0232da756e50403119393f46790803f743b6
NectDz <54990613+NectDz@users.noreply.github.com>
2022-01-12 20:06:40 -0500

    Update eventCommands.py

--- cogs/eventCommands.py
+++ cogs/eventCommands.py
@@ -60,9 +60,11 @@ class eventCommands(commands.Cog):
         if message.content.lower() == "the younger the better":
             await message.channel.send(f"Exactly!")
 
+
+        """
         if message.author.id == 825096759063216128: 
             await message.channel.purge(limit=1)
-
+        """
 
     # CoolDowns
 
